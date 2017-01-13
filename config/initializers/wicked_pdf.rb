13bcb7056f5311b7a54c47bc39214005dd3c0e77
alejosaldarriagaalzate <alejoalzate@misena.edu.co>
2017-01-13 11:50:55 -0500

    pdf1

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -14,13 +14,7 @@ WickedPdf.config = {
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-WickedPdf.config = {
-  :layout => 'layout',
-  :margin => {
-    :top => 38,
-    :bottom => 25
-  }
-}
+
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
