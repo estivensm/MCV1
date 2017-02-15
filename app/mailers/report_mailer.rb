890ba239012d303760bd35c5ff58cd9dfd298426
alejosaldarriagaalzate <alejoalzate@misena.edu.co>
2017-02-15 11:30:10 -0500

    correos1

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -2,7 +2,7 @@ class ReportMailer < ApplicationMailer
 	def noty_report(employed , report)
   @employed = employed
   @report = report
-  mail(to: @employed.email, subject: "Los productos top de ayer")
+  mail(to: @employed.email, subject: "Se creo el reporte #{@report.codigo}")
 end
 end
 
