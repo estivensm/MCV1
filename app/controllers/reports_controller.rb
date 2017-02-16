55764cd6e494f55a912d2d8bf358df72e3584881
alejosaldarriagaalzate <alejoalzate@misena.edu.co>
2017-02-16 14:49:38 -0500

    mejorando apariencia

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -77,10 +77,10 @@ end
 
                 #@reports2 = @reports1.where(admin_user: current_user.admin_user).where(proceso_id: a.cargo.proceso_id ).or(Report.where(employed_id: a.id )).order(created_at: :desc)
                 @reports2 = @reports1.where(admin_user: current_user.admin_user).where(proceso_id: a.cargo.proceso_id ).order(created_at: :desc)
-                @reports = @reports2.paginate(page: params[:page],:per_page => 10)
+                @reports = @reports2.paginate(page: params[:page],:per_page => 3)
                 
               else
-                    @reports = @reports1.paginate(page: params[:page],:per_page => 10).where(admin_user: current_user.admin_user).order(created_at: :desc)
+                    @reports = @reports1.paginate(page: params[:page],:per_page => 3).where(admin_user: current_user.admin_user).order(created_at: :desc)
 
               end
 
