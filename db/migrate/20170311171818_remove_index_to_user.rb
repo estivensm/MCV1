937bf19aaac4715315421bb002a23b73517fe211
alejosaldarriagaalzate <alejoalzate@misena.edu.co>
2017-03-11 14:08:00 -0500

    username

--- db/migrate/20170311171818_remove_index_to_user.rb
+++ db/migrate/20170311171818_remove_index_to_user.rb
@@ -1,7 +1,7 @@
 class RemoveIndexToUser < ActiveRecord::Migration[5.0]
   def change
   	  remove_index :users, :email
-  	  remove_index  :users, :name
+  	 # remove_index  :users, :name
     
   end
 end
