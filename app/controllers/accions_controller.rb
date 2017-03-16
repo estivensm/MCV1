a72dccc6c3218474a8460fc475586ac2edd2cf10
alejosaldarriagaalzate <alejoalzate@misena.edu.co>
2017-03-16 17:04:29 -0500

    tipo

--- app/controllers/accions_controller.rb
+++ app/controllers/accions_controller.rb
@@ -39,7 +39,7 @@ class AccionsController < ApplicationController
 
   # GET /accions/1/edit
   def edit
-     @tipo = params[:tipo]
+     @tipo = @accion.tipo
      @report = Report.find(params[:report_id]) 
   end
 
