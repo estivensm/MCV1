ce07368d49d82aa927b418c93961e6c1df89b625
alejovelez10 <alejovelez10@gmail.com>
2017-03-30 09:44:36 -0500

    arreglando dasboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,8 @@ class DashboardController < ApplicationController
  before_action :authenticate_user!
   def index
     @employed = Employed.where(email: current_user.email).where(admin_user: current_user.admin_user).first
-  	@reports = Report.where(employed_id:@employed.id).limit(3).order(f_compromiso: :asc)
-  	@accions = Accion.where(employed_id:@employed.id).limit(3).order(f_compromiso: :asc)
+  	@reports = Report.where(employed_id:@employed.id).where("contador_seg <= ?", 5).order(f_compromiso: :asc)
+  	@accions = Accion.where(employed_id:@employed.id).where("contador_seg <= ?", 5).order(f_compromiso: :asc)
   end
 end
+
