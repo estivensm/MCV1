4008f587c1897a610ef429a3c3322e8e721dbd6e
alejovelez10 <alejovelez10@gmail.com>
2017-03-30 09:57:26 -0500

    add contador dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,10 @@ class DashboardController < ApplicationController
     @employed = Employed.where(email: current_user.email).where(admin_user: current_user.admin_user).first
   	@reports = Report.where(employed_id:@employed.id).where("contador_seg <= ?", 5).order(f_compromiso: :asc)
   	@accions = Accion.where(employed_id:@employed.id).where("contador_seg <= ?", 5).order(f_compromiso: :asc)
+  	@reportspv = Report.where(employed_id:@employed.id).where("contador_seg <= ? AND contador_seg >= ? ", 5,0)
+  	@accionspv = Accion.where(employed_id:@employed.id).where("contador_seg <= ? AND contador_seg >= ? ", 5,0)
+  	@reportsv = Report.where(employed_id:@employed.id).where("contador_seg < ?", 0)
+  	@accionsv = Accion.where(employed_id:@employed.id).where("contador_seg < ?", 0)
   end
 end
 
