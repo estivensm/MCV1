b4572aeb691bd540b335da62c7987613b250e848
alejovelez10 <alejovelez10@gmail.com>
2018-10-16 11:21:04 -0500

    Organizando correo de bienvenida

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,6 +1,6 @@
 class WelcomeMailer < ApplicationMailer
 	def welcome(user)
   @user = user
- mail(to: @user.email, subject: "Bienvenido a Visualisoft")
+ mail(to: @user.email, subject: "Bienvenido a MejoraApp")
 end
 end
